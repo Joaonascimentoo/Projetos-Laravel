f759612f891c81e6c1b1a25fc88b390bf75b5195
Joao Muniz <joao.muniz@sesatech.com.br>
2022-09-03 15:21:00 -0300

    fix btns in the index page

--- controle-series/resources/views/series/index.blade.php
+++ controle-series/resources/views/series/index.blade.php
@@ -12,23 +12,23 @@
         <li class="list-group-item d-flex justify-content-between align-items-center">
             @auth<a href="{{ route ('seasons.index', $serie->id) }}">@endauth
                 {{ $serie->nome }}
-            @auth</a>@endauth
+                @auth</a>@endauth
             @auth
             <span class="d-flex">
                 <a href="{{ route('series.edit',$serie->id)}}" class="btn- btn-primary btn-sm">
                     E
                 </a>
+
+                <form action="{{ route('series.destroy',$serie->id) }}" method="post" class="ms-2">
+                    @csrf
+                    @method ('DELETE')
+
+                    <button class="btn btn-danger btn-sm">
+                        X
+                    </button>
+                </form>
+                @endauth
             </span>
-
-            <form action="{{ route('series.destroy',$serie->id) }}" method="post" class="ms-2">
-                @csrf
-                @method ('DELETE')
-
-                <button class="btn btn-danger btn-sm">
-                    X
-                </button>
-            </form>
-            @endauth
         </li>
         @endforeach
     </ul>
